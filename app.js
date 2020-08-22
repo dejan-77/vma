a0362f0a1c5029e39ffc2231ce51fdbb87f1c404
dejan-77 <dejankutic@me.com>
2020-08-22 16:48:37 +0200

    fef

--- app.js
+++ app.js
@@ -124,9 +124,10 @@ animation
 
 .from(".nav-line-1", {scaleX:0, transformOrigin: "left center"})
 .from(".nav-line-2", {scaleX:0, transformOrigin: "left center"}, "<0.1")
-.from(".home-img-wrap", {yPercent:50}, "<")
-.from(".triangle-wrap", {y:500}, "<0.1")
 .from(".char", {stagger:0.02, yPercent:105}, "<0.1")
+.from(".home-img-wrap", {yPercent:50}, "<0.1")
+.from(".triangle-wrap", {yPercent:150}, "<0.1")
+
 
 
 
