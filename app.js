3f03ca899711cd6ccf5bc7ac30e0a499c7c9dffd
dejan-77 <dejankutic@me.com>
2020-08-22 20:10:23 +0200

    egerg

--- app.js
+++ app.js
@@ -132,7 +132,7 @@ info
 .set(".zero", {autoAlpha:1})
 
 .from(".word", {stagger:0.02, rotate:30, y:100, opacity:0})
-.fromTo(".info-img-mask", {clipPath:"polygon(0 0, 100% 0, 100% 100%, 0% 100%)"}, 
+.fromTo(".info-img-mask", {clipPath:"polygon(100% 0, 100% 0, 100% 100%, 100% 100%)"}, 
 {duration: 0.5, ease: "power1.out", clipPath:"polygon(100% 0, 100% 0, 100% 100%, 100% 100%)"})  
 
 
