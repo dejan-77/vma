c5b2172cb908b4deef77319ba75a67029faa5574
dejan-77 <dejankutic@me.com>
2020-08-29 10:54:39 +0200

    gettersetter updated

--- app.js
+++ app.js
@@ -53,6 +53,8 @@ var pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
 var mouse = { x: pos.x, y: pos.y };
 var speed = 0.1;
 
+var fpms = 60 / 1000;
+
 var xSet = gsap.quickSetter(ball, "x", "px");
 var ySet = gsap.quickSetter(ball, "y", "px");
 
@@ -61,8 +63,10 @@ window.addEventListener("mousemove", e => {
   mouse.y = e.y;  
 });
 
-gsap.ticker.add(() => {
-  var dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio()); 
+gsap.ticker.add((time, deltaTime) => {
+  
+	var delta = deltaTime * fpms;
+	var dt = 1.0 - Math.pow(1.0 - speed, delta); 
   
   pos.x += (mouse.x - pos.x) * dt;
   pos.y += (mouse.y - pos.y) * dt;
